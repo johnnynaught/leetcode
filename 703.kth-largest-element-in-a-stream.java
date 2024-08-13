9436ccdf27d6fca86fe627b66bd0367c2db3860c
Johnny <johnnyif@foxmail.com>
2024-08-12 22:52:34 -0400

    2024.08.12

---
+++ 703.kth-largest-element-in-a-stream.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=703 lang=java
+ *
+ * [703] Kth Largest Element in a Stream
+ */
+
+// @lc code=start
+class KthLargest {
+    PriorityQueue<Integer> minHeap;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        minHeap = new PriorityQueue<>();
+        this.k = k;
+        for(int num : nums){
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        if(minHeap.size() < k || minHeap.peek() < val){
+            minHeap.add(val);
+            if(minHeap.size() > k){
+                minHeap.remove();
+            }
+        }
+        return minHeap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
+// @lc code=end
+
