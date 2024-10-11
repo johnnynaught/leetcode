f920b1c272e8c1aae7d39868d7018ac65bca1278
Johnny <johnnyif@foxmail.com>
2024-10-11 19:53:37 -0400

    2024.10.11

---
+++ 74.search-a-2-d-matrix.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=74 lang=python3
+#
+# [74] Search a 2D Matrix
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        x = 0
+        y = len(matrix[0]) - 1
+
+        while x < len(matrix) and y >= 0:
+            if matrix[x][y] == target:
+                return True
+            elif matrix[x][y] > target:
+                y -= 1
+            else:
+                x += 1
+
+        return False
+# @lc code=end
+
